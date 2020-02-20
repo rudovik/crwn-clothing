9afc766b5083f37037aa12aeec5d9cd572d98065
rudovik <flashwrite@ukr.net>
2020-02-20 13:52:28 +0200

    Added checkout page, checkoutItem component and updated cart to use selectors.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -29,7 +35,7 @@ class App extends React.Component {
           });
         });
       } else {
-        setCurrentUser(userAuth);
+        setCurrentUser(null);
       }
     });
   }
@@ -45,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -62,10 +69,15 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({ currentUser: user.currentUser });
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
+
+const mapDispatchToProps = dispatch => {
+  console.log('mapDispatchToProps was invoked.');
+  return {
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
